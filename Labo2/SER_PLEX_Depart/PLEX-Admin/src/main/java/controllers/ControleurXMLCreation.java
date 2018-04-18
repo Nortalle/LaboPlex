2ebb0a0cddb6181952ebfc93b8d87d2e9cb35bc2
Nortalle <vincent.guidoux1@heig-vd.ch>
2018-04-18 18:30:04 +0200

    partos

--- Labo2/SER_PLEX_Depart/PLEX-Admin/src/main/java/controllers/ControleurXMLCreation.java
+++ Labo2/SER_PLEX_Depart/PLEX-Admin/src/main/java/controllers/ControleurXMLCreation.java
@@ -27,124 +27,181 @@ import com.thoughtworks.xstream.XStream;
 
 public class ControleurXMLCreation {
 
-	//private ControleurGeneral ctrGeneral;
-	private static MainGUI mainGUI;
-	private ORMAccess ormAccess;
+    //private ControleurGeneral ctrGeneral;
+    private static MainGUI mainGUI;
+    private ORMAccess ormAccess;
 
-	private GlobalData globalData;
+    private GlobalData globalData;
 
-	public ControleurXMLCreation(ControleurGeneral ctrGeneral, MainGUI mainGUI, ORMAccess ormAccess){
-		//this.ctrGeneral=ctrGeneral;
-		ControleurXMLCreation.mainGUI=mainGUI;
-		this.ormAccess=ormAccess;
-	}
+    public ControleurXMLCreation(ControleurGeneral ctrGeneral, MainGUI mainGUI, ORMAccess ormAccess) {
+        //this.ctrGeneral=ctrGeneral;
+        ControleurXMLCreation.mainGUI = mainGUI;
+        this.ormAccess = ormAccess;
+    }
 
-	public void createXML(){
-		new Thread(){
-				public void run(){
-					mainGUI.setAcknoledgeMessage("Creation XML... WAIT");
-					long currentTime = System.currentTimeMillis();
-					try {
-						globalData = ormAccess.GET_GLOBAL_DATA();
-						try {
-							BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("xml.xml"), "UTF-8"));
-							out.write("<?xml version=\"1.0\" ?>\n");
-							out.write("<!DOCTYPE projections SYSTEM \"Plex.dtd\">\n");
-							out.write("<projections>\n");
-							for (Projection p:globalData.getProjections()){
-								out.write("\t<projection>\n");
-								{
-									out.write("\t\t<date>\n");
-									Calendar date = p.getDateHeure();
-									{
-										out.write("\t\t\t<jour>" + date.get(Calendar.DAY_OF_MONTH) + "</jour>\n");
-										out.write("\t\t\t<mois>" + date.get(Calendar.MONTH) + "</mois>\n");
-										out.write("\t\t\t<annee>" + date.get(Calendar.YEAR) + "</annee>\n");
-									}
-									out.write("\t\t<date>\n");
-								}
+    public void createXML() {
+        new Thread() {
+            public void run() {
+                mainGUI.setAcknoledgeMessage("Creation XML... WAIT");
+                long currentTime = System.currentTimeMillis();
+                try {
+                    globalData = ormAccess.GET_GLOBAL_DATA();
+                    try {
+                        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("plex.xml"), "UTF-8"));
+                        out.write("<?xml version=\"1.0\" ?>\n");
+                        out.write("<!DOCTYPE projections SYSTEM \"Plex.dtd\">\n");
+                        out.write("<projections>\n");
+                        for (Projection p : globalData.getProjections()) {
+                            out.write("\t<projection>\n");
+                            {
+                                out.write("\t\t<date>\n");
+                                Calendar date = p.getDateHeure();
+                                {
+                                    out.write("\t\t\t<jour>" + date.get(Calendar.DAY_OF_MONTH) + "</jour>\n");
+                                    out.write("\t\t\t<mois>" + date.get(Calendar.MONTH) + "</mois>\n");
+                                    out.write("\t\t\t<annee>" + date.get(Calendar.YEAR) + "</annee>\n");
+                                    out.write("\t\t\t<heure>" + date.get(Calendar.HOUR_OF_DAY) + "</heure>\n");
+                                    out.write("\t\t\t<minute>" + date.get(Calendar.MINUTE) + "</minute>\n");
+                                }
+                                out.write("\t\t<date>\n");
+                            }
+                            {
+                                out.write("\t\t<salle>" + p.getSalle().getNo() + "</salle>\n");
+                            }
+                            {
+                                out.write("\t\t<film>\n");
+                                {
+                                    out.write("\t\t\t<titre>" + p.getFilm().getTitre() + "</titre>\n");
+                                    out.write("\t\t\t<synopsis>" + p.getFilm().getSynopsis() + "</synopsis>\n");
+                                    out.write("\t\t\t<duree>" + p.getFilm().getDuree() + "</duree>\n");
 
-								out.write("\t</projection>\n");
-							}
-							out.write("</projections>\n");
-							out.close();
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
-					catch (Exception e){
-						mainGUI.setErrorMessage("Construction XML impossible", e.toString());
-					}
-				}
-		}.start();
-	}
+                                    out.write("\t\t\t<critiques>\n");
+                                    for (Critique c : p.getFilm().getCritiques()) {
+                                        out.write("\t\t\t\t<critique>\n");
+                                        {
+                                            out.write("\t\t\t\t\t<texte>" + c.getTexte() + "</texte>\n");
+                                            out.write("\t\t\t\t\t<note>" + c.getNote() + "</note>\n");
+                                        }
+                                        out.write("\t\t\t\t</critique>\n");
+                                    }
+                                    out.write("\t\t\t<critiques>\n");
 
-	public void createXStreamXML(){
-		new Thread(){
-				public void run(){
-					mainGUI.setAcknoledgeMessage("Creation XML... WAIT");
-					long currentTime = System.currentTimeMillis();
-					try {
-						globalData = ormAccess.GET_GLOBAL_DATA();
-						globalDataControle();
-					}
-					catch (Exception e){
-						mainGUI.setErrorMessage("Construction XML impossible", e.toString());
-					}
 
-					XStream xstream = new XStream();
-					writeToFile("global_data.xml", xstream, globalData);
-					System.out.println("Done [" + displaySeconds(currentTime, System.currentTimeMillis()) + "]");
-					mainGUI.setAcknoledgeMessage("XML cree en "+ displaySeconds(currentTime, System.currentTimeMillis()) );
-				}
-		}.start();
-	}
+                                    out.write("\t\t\t<genres>\n");
+                                    for(Genre g : p.getFilm().getGenres()){
+                                        out.write("\t\t\t\t<genre>");
+                                        {
+                                            out.write(g.getLabel());
+                                        }
+                                        out.write("</genre>\n");
+                                    }
+                                    out.write("\t\t\t</genres>\n");
 
-	private static void writeToFile(String filename, XStream serializer, Object data) {
-		try {
-			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename), "UTF-8"));
-			serializer.toXML(data, out);
-			out.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private static final DecimalFormat doubleFormat = new DecimalFormat("#.#");
-	private static final String displaySeconds(long start, long end) {
-		long diff = Math.abs(end - start);
-		double seconds = ((double) diff) / 1000.0;
-		return doubleFormat.format(seconds) + " s";
-	}
+                                    out.write("\t\t\t<mots_cles>\n");
+                                    for(Motcle mc : p.getFilm().getMotcles()){
+                                        out.write("\t\t\t\t<mot_cle>");
+                                        {
+                                            out.write(mc.getLabel());
+                                        }
+                                        out.write("</mot_cle>\n");
+                                    }
+                                    out.write("\t\t\t</mots_cles>\n");
 
-	private void globalDataControle(){
-		for (Projection p:globalData.getProjections()){
-			System.out.println("******************************************");
-			System.out.println(p.getFilm().getTitre());
-			System.out.println(p.getSalle().getNo());
-			System.out.println("Acteurs *********");
-			for(RoleActeur role : p.getFilm().getRoles()) {
-				System.out.println(role.getActeur().getNom());
-			}
-			System.out.println("Genres *********");
-			for(Genre genre : p.getFilm().getGenres()) {
-				System.out.println(genre.getLabel());
-			}
-			System.out.println("Mot-cles *********");
-			for(Motcle motcle : p.getFilm().getMotcles()) {
-				System.out.println(motcle.getLabel());
-			}
-			System.out.println("Langages *********");
-			for(Langage langage : p.getFilm().getLangages()) {
-				System.out.println(langage.getLabel());
-			}
-			System.out.println("Critiques *********");
-			for(Critique critique : p.getFilm().getCritiques()) {
-				System.out.println(critique.getNote());
-				System.out.println(critique.getTexte());
-			}
-		}
-	}
+                                    out.write("\t\t\t<langages>\n");
+                                    for(Langage l : p.getFilm().getLangages()){
+                                        out.write("\t\t\t\t<langue>");
+                                        {
+                                            out.write(l.getLabel());
+                                        }
+                                        out.write("</langue>\n");
+                                    }
+                                    out.write("\t\t\t</langages>\n");
+
+
+                                }
+                                out.write("\t\t</film>\n");
+                            }
+
+                            out.write("\t</projection>\n");
+                        }
+                        out.write("</projections>\n");
+                        out.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    mainGUI.setErrorMessage("Construction XML impossible", e.toString());
+                }
+            }
+        }.start();
+    }
+
+    public void createXStreamXML() {
+        new Thread() {
+            public void run() {
+                mainGUI.setAcknoledgeMessage("Creation XML... WAIT");
+                long currentTime = System.currentTimeMillis();
+                try {
+                    globalData = ormAccess.GET_GLOBAL_DATA();
+                    globalDataControle();
+                } catch (Exception e) {
+                    mainGUI.setErrorMessage("Construction XML impossible", e.toString());
+                }
+
+                XStream xstream = new XStream();
+                writeToFile("global_data.xml", xstream, globalData);
+                System.out.println("Done [" + displaySeconds(currentTime, System.currentTimeMillis()) + "]");
+                mainGUI.setAcknoledgeMessage("XML cree en " + displaySeconds(currentTime, System.currentTimeMillis()));
+            }
+        }.start();
+    }
+
+    private static void writeToFile(String filename, XStream serializer, Object data) {
+        try {
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename), "UTF-8"));
+            serializer.toXML(data, out);
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static final DecimalFormat doubleFormat = new DecimalFormat("#.#");
+
+    private static final String displaySeconds(long start, long end) {
+        long diff = Math.abs(end - start);
+        double seconds = ((double) diff) / 1000.0;
+        return doubleFormat.format(seconds) + " s";
+    }
+
+    private void globalDataControle() {
+        for (Projection p : globalData.getProjections()) {
+            System.out.println("******************************************");
+            System.out.println(p.getFilm().getTitre());
+            System.out.println(p.getSalle().getNo());
+            System.out.println("Acteurs *********");
+            for (RoleActeur role : p.getFilm().getRoles()) {
+                System.out.println(role.getActeur().getNom());
+            }
+            System.out.println("Genres *********");
+            for (Genre genre : p.getFilm().getGenres()) {
+                System.out.println(genre.getLabel());
+            }
+            System.out.println("Mot-cles *********");
+            for (Motcle motcle : p.getFilm().getMotcles()) {
+                System.out.println(motcle.getLabel());
+            }
+            System.out.println("Langages *********");
+            for (Langage langage : p.getFilm().getLangages()) {
+                System.out.println(langage.getLabel());
+            }
+            System.out.println("Critiques *********");
+            for (Critique critique : p.getFilm().getCritiques()) {
+                System.out.println(critique.getNote());
+                System.out.println(critique.getTexte());
+            }
+        }
+    }
 }
 
 
